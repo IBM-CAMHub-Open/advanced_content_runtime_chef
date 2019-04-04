c7578d78633313267aeb8e52f84fbe23f7e1daa4
CAMHub PublishBot <octravis@us.ibm.com>
2019-04-04 17:03:56 +0000

    Automated CAMHub::publish on 2019-04-04 17:03:56 UTC

--- content_runtime_template/vmware/bring_your_own_chef/content_runtime.tf
+++ content_runtime_template/vmware/bring_your_own_chef/content_runtime.tf
@@ -337,6 +337,7 @@ resource "vsphere_virtual_machine" "singlenode" {
   folder           = "${var.vsphere_folder}"
   resource_pool_id = "${data.vsphere_resource_pool.input_resource_pool.id}"
   guest_id         = "${data.vsphere_virtual_machine.input_template.guest_id}"
+  scsi_type        = "${data.vsphere_virtual_machine.input_template.scsi_type}"
 
   # wait_for_guest_net = false
 
@@ -2171,6 +2172,12 @@ fi
 echo "[*] Template Timestamp: $TEMPLATE_TIMESTAMP"
 end_message "Successful"
 
+begin_message "Set user provided ssh key."
+# Add the users key to the authorized keys on the system
+[[ ! -e ~/.ssh ]] && { mkdir -p ~/.ssh/ ; chmod 700 ~/.ssh; }
+echo $USER_PUBLIC_KEY >> ~/.ssh/authorized_keys
+end_message "Successful"
+
 begin_message "Requirements Checker"
 # Check and install pre-requisites
 chmod +x $runtimepath/prereq-check-install.sh
@@ -2406,11 +2413,6 @@ else
     echo "[*] Pattern Manager configuration file already exists in the path"
 fi
 
-# Add the users key to the authorized keys on the system
-[[ ! -e ~/.ssh ]] && { mkdir -p ~/.ssh/ ; chmod 700 ~/.ssh; }
-echo $USER_PUBLIC_KEY >> ~/.ssh/authorized_keys
-end_message "Successful"
-
 begin_message "Software Repository"
 AUTH_FILE_PATH="/opt/ibm/docker/software-repo/etc/nginx/auth"
 if [[ ! -z "$SOFTWARE_REPO_PASS" ]];  then
@@ -2604,7 +2606,7 @@ resource "null_resource" "call_launcher" {
     inline = [
       "chmod 775 ./advanced-content-runtime/launch-docker-compose.sh",
       "chmod 775 ./advanced-content-runtime/image-upgrade.sh",
-      "bash -c \"./advanced-content-runtime/launch-docker-compose.sh ${var.network_visibility} --docker_registry_token='${var.docker_registry_token}' --nfs_mount_point='${var.nfs_mount}' --encryption_passphrase='${var.encryption_passphrase}' --software_repo_user='${var.ibm_sw_repo_user}' --software_repo_pass='${var.ibm_sw_repo_password}' --im_repo_user='${var.ibm_im_repo_user_hidden}' --im_repo_pass='${var.ibm_im_repo_password_hidden}'  --chef_host=chef-server --software_repo=software-repo --software_repo_port='${var.ibm_sw_repo_port}' --software_repo_secure_port='${var.ibm_sw_repo_secure_port}' --pattern_mgr=pattern --ibm_contenthub_git_host='${var.ibm_contenthub_git_host}' --ibm_contenthub_git_organization='${var.ibm_contenthub_git_organization}' --ibm_openhub_git_organization='${var.ibm_openhub_git_organization}' --chef_org='${var.chef_org}' --chef_admin='${var.chef_admin}' --chef_fqdn='${var.chef_fqdn}' --chef_ip='${var.chef_ip}' --chef_pem='${var.chef_pem}' --docker_registry='${var.docker_registry}' --chef_version=${var.chef_version} --chef_client_version='${var.chef_client_version}' --chef_client_path='${var.chef_client_path}' --ibm_pm_access_token='${var.ibm_pm_access_token}' --ibm_pm_admin_token='${var.ibm_pm_admin_token}' --camc-sw-repo_version='${var.docker_registry_camc_sw_repo_version}' --docker_ee_repo='${var.docker_ee_repo}' --camc-pattern-manager_version='${var.docker_registry_camc_pattern_manager_version}' --docker_configuration=single-node --ibm_pm_public_ssh_key_name='${var.ibm_pm_public_ssh_key_name}' --ibm_pm_private_ssh_key='${var.ibm_pm_private_ssh_key}' --ibm_pm_public_ssh_key='${var.ibm_pm_public_ssh_key}' --user_public_ssh_key='${var.user_public_ssh_key}' --prereq_strictness='${var.prereq_strictness}' --ip_address='${var.ipv4_address}' --template_timestamp='${var.template_timestamp_hidden}' --installer_docker='${var.installer_docker}' --installer_docker_compose='${var.installer_docker_compose}' --sw_repo_image='${var.sw_repo_image}' --pm_image='${var.pm_image}' --template_debug='${var.template_debug}' --portable_private_ip='${var.portable_private_ip}' --byochef='${var.byochef}' --offline_installation='${var.offline_installation}' --install_cookbooks='${var.install_cookbooks}'\"",
+      "bash -c \"./advanced-content-runtime/launch-docker-compose.sh ${var.network_visibility} --docker_registry_token='\"'${var.docker_registry_token}'\"' --nfs_mount_point='\"'${var.nfs_mount}'\"' --encryption_passphrase='\"'${var.encryption_passphrase}'\"' --software_repo_user='\"'${var.ibm_sw_repo_user}'\"' --software_repo_pass='\"'${var.ibm_sw_repo_password}'\"' --im_repo_user='\"'${var.ibm_im_repo_user_hidden}'\"' --im_repo_pass='\"'${var.ibm_im_repo_password_hidden}'\"'  --chef_host=chef-server --software_repo=software-repo --software_repo_port='\"'${var.ibm_sw_repo_port}'\"' --software_repo_secure_port='\"'${var.ibm_sw_repo_secure_port}'\"' --pattern_mgr=pattern --ibm_contenthub_git_host='\"'${var.ibm_contenthub_git_host}'\"' --ibm_contenthub_git_organization='\"'${var.ibm_contenthub_git_organization}'\"' --ibm_openhub_git_organization='\"'${var.ibm_openhub_git_organization}'\"' --chef_org='\"'${var.chef_org}'\"' --chef_admin='\"'${var.chef_admin}'\"' --chef_fqdn='\"'${var.chef_fqdn}'\"' --chef_ip='\"'${var.chef_ip}'\"' --chef_pem='\"'${var.chef_pem}'\"' --docker_registry='\"'${var.docker_registry}'\"' --chef_version=${var.chef_version} --chef_client_version='\"'${var.chef_client_version}'\"' --chef_client_path='\"'${var.chef_client_path}'\"' --ibm_pm_access_token='\"'${var.ibm_pm_access_token}'\"' --ibm_pm_admin_token='\"'${var.ibm_pm_admin_token}'\"' --camc-sw-repo_version='\"'${var.docker_registry_camc_sw_repo_version}'\"' --docker_ee_repo='\"'${var.docker_ee_repo}'\"' --camc-pattern-manager_version='\"'${var.docker_registry_camc_pattern_manager_version}'\"' --docker_configuration=single-node --ibm_pm_public_ssh_key_name='\"'${var.ibm_pm_public_ssh_key_name}'\"' --ibm_pm_private_ssh_key='\"'${var.ibm_pm_private_ssh_key}'\"' --ibm_pm_public_ssh_key='\"'${var.ibm_pm_public_ssh_key}'\"' --user_public_ssh_key='\"'${var.user_public_ssh_key}'\"' --prereq_strictness='\"'${var.prereq_strictness}'\"' --ip_address='\"'${var.ipv4_address}'\"' --template_timestamp='\"'${var.template_timestamp_hidden}'\"' --installer_docker='\"'${var.installer_docker}'\"' --installer_docker_compose='\"'${var.installer_docker_compose}'\"' --sw_repo_image='\"'${var.sw_repo_image}'\"' --pm_image='\"'${var.pm_image}'\"' --template_debug='\"'${var.template_debug}'\"' --portable_private_ip='\"'${var.portable_private_ip}'\"' --byochef='\"'${var.byochef}'\"' --offline_installation='\"'${var.offline_installation}'\"' --install_cookbooks='\"'${var.install_cookbooks}'\"'\"",
     ]
   }
 } # End of null_resource
